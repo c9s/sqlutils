84b7cb326fc8faa3e9f83759b7765b02bd4fe276
c9s <cornelius.howl@gmail.com>
2013-04-11 16:16:44 +0800

    Import checker test

--- reflect_test.go
+++ reflect_test.go
@@ -41,23 +41,3 @@ func TestColumnNamesParsing(t * testing.T) {
 	}
 }
 
-
-
-func TestRequireChecking2(t *testing.T) {
-	staff := Staff{Id:4, Name: "John", Gender: "m", Phone: "0975277696"}
-	err := CheckRequired(&staff)
-	if err != nil {
-		t.Fatal("Name field is required.")
-	}
-}
-
-func TestRequireChecking(t *testing.T) {
-	staff := Staff{Id:4, Gender: "m", Phone: "0975277696"}
-	err := CheckRequired(&staff)
-	if err == nil {
-		t.Fatal("Name field should be required.")
-	}
-}
-
-
-
