1325792e1a7ceae604cf0b1b9e0704c378266543
c9s <cornelius.howl@gmail.com>
2013-04-12 00:30:52 +0800

    comment

--- select_test.go
+++ select_test.go
@@ -8,6 +8,8 @@ type Staff struct {
 	StaffType string `json:"staff_type"` // valid types: doctor, nurse, ...etc
 	Phone     string `json:"phone"`
 }
+
+// Implement the GetPkId interface
 func (self *Staff) GetPkId() int64 {
 	return self.Id
 }
@@ -22,7 +24,6 @@ func TestBuildSelectColumns(t * testing.T) {
 	}
 }
 
-
 func TestBuildSelectClause(t * testing.T) {
 	staff := Staff{Id:4, Name: "John", Gender: "m", Phone: "0975277696"}
 	sql := BuildSelectClause(&staff)
@@ -30,3 +31,4 @@ func TestBuildSelectClause(t * testing.T) {
 		t.Fatal(sql)
 	}
 }
+
