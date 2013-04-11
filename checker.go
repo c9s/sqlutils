302e64438c66afddc237e18105ed2cdd6bdae558
c9s <cornelius.howl@gmail.com>
2013-04-11 14:26:31 +0800

    check integer

--- checker.go
+++ checker.go
@@ -17,6 +17,8 @@ func CheckRequired(val interface{}) (error) {
 			// check the column value
 			if fieldType.String() == "string" && field.Interface().(string) == "" {
 				return errors.New("string field required.")
+			} else if fieldType.String() == "int" && field.Interface().(int) == 0 {
+				return errors.New("int field required.")
 			}
 		}
 	}
