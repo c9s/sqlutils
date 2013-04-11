d1be8c9e4825c41a993011aa8d4b76935619bd0a
c9s <cornelius.howl@gmail.com>
2013-04-11 13:31:48 +0800

    Fix indentation

--- where.go
+++ where.go
@@ -4,14 +4,14 @@ import "strings"
 
 
 func BuildWhereClauseFromMap(argMap map[string]interface{}) (string, []interface{}) {
-		var fields []string
-		var args   []interface{}
-		var i int = 1
-		for name, val := range argMap {
-				fields = append(fields, fmt.Sprintf("%s = $%d", name, i) )
-				args   = append(args, val)
-				i++
-		}
-		return " WHERE " + strings.Join(fields, " AND "), args
+	var fields []string
+	var args   []interface{}
+	var i int = 1
+	for name, val := range argMap {
+		fields = append(fields, fmt.Sprintf("%s = $%d", name, i) )
+		args   = append(args, val)
+		i++
+	}
+	return " WHERE " + strings.Join(fields, " AND "), args
 }
 
